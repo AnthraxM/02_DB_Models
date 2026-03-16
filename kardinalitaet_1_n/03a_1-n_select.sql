89442bdf1ff5df94b0a42a327f7a224b1d7c9394
Peter <schmidt.multimedia@gmail.com>
2026-03-16 11:10:29 +0100

    01a/ n-m Beispiele JOIN

--- kardinalitaet_1_n/03a_1-n_select.sql
+++ kardinalitaet_1_n/03a_1-n_select.sql
@@ -2,24 +2,24 @@
 # 04 SELECTS
 
 # Einzeltabellen
-SELECT * FROM design.catmoms;
-SELECT * FROM design.kittens;
+-- SELECT * FROM design.catmoms;
+-- SELECT * FROM design.kittens;
 
 # Inner Join 1 / Gesamte Kombi-Tabelle
-SELECT
-    *
-FROM design.catmoms INNER JOIN design.kittens
-ON design.catmoms.id = design.kittens.catmoms_id
-; 
+-- SELECT
+--     *
+-- FROM design.catmoms INNER JOIN design.kittens
+-- ON design.catmoms.id = design.kittens.catmoms_id
+-- ; 
 
 # Inner Join 2 / Verwandschaft
 # "X ist die Mutter von Y"
-SELECT
-    catmoms.cat_name AS Katzen,
-    CONCAT(catmoms.cat_name, " ist die Mutter von ", kittens.kitten_name) Verwandschaft
-FROM design.catmoms INNER JOIN design.kittens
-ON design.catmoms.id = design.kittens.catmoms_id
-;
+-- SELECT
+--     catmoms.cat_name AS Katzen,
+--     CONCAT(catmoms.cat_name, " ist die Mutter von ", kittens.kitten_name) Verwandschaft
+-- FROM design.catmoms INNER JOIN design.kittens
+-- ON design.catmoms.id = design.kittens.catmoms_id
+-- ;
 
 # Inner Join 3 / Wieviel Kinder haben die Katzen?
 SELECT
--- kardinalitaet_n_m_I/03a_n-m_select.sql
+++ kardinalitaet_n_m_I/03a_n-m_select.sql
@@ -1,43 +1,99 @@
+\! cls
+
 -- 03. SELECTS / Servants, Products, Purchases
 
--- Einzeltabellen
+# Einzeltabellen
+-- SELECT * FROM design.servants;
+-- SELECT * FROM design.products;
+-- SELECT * FROM design.purchases;
 
+# Inner Join 1 / Kombi (servants / products / purchases)
+-- SELECT
+--     *
+-- FROM design.purchases
+-- INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+-- INNER JOIN design.products ON design.products.id = design.purchases.products_id
+-- ;
 
--- Inner Join 1 / Kombi (servants / products / purchases)
+# Welche Artikel hat X/Y gekauft?
+SELECT
+    servant_name AS Diener,
+    product_name AS "Von X gekaufte Produkte:",
+    CONCAT(servant_name, " kauft ", product_name, ".") AS Kaufhandlung
+FROM design.purchases
+INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+INNER JOIN design.products ON design.products.id = design.purchases.products_id
+-- WHERE servant_name = "Max"
+-- WHERE servant_name LIKE "%ine"
+;
 
+# Wieviele Produkte hat X gekauft?
+# "X kauft Y Produkte"
+# Variante 1
+-- SELECT
+--     servant_name AS "Diener",
+--     count(product_name) AS Produkt
+-- FROM design.purchases
+-- INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+-- INNER JOIN design.products ON design.products.id = design.purchases.products_id
+-- GROUP BY servant_name
+-- ; 
 
+# Variante 2
+-- SELECT
+--     CONCAT(servant_name, " kauft ", COUNT(product_name), " Produkte.") AS Kaufhandlung
+-- FROM design.purchases
+-- INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+-- INNER JOIN design.products ON design.products.id = design.purchases.products_id
+-- GROUP BY servant_name -- organisch / aggregiert
+-- -- HAVING servant_name = "Max"
+-- HAVING servant_name LIKE "%ine"
+-- ;
 
--- Welche Artikel hat X/Y gekauft?
+# Wieviel Geld hat jeder Diener ausgegeben?
+-- SELECT
+--     servant_name AS "Diener",
+--     SUM(product_price) AS "Gesamtkosten (Euro)",
+--     -- AVG(product_price) AS Durchschnittspreis
+-- FROM design.purchases
+-- INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+-- INNER JOIN design.products ON design.products.id = design.purchases.products_id
+-- GROUP BY servant_name
+-- ; 
 
+# Wer hat das Produkt X gekauft?  
+# Irgendwas mit Lachs / Irgendwas mit Sauce LIKE
+# Spalten --> Diener / Produkt
+# WHERE / LIKE
+-- SELECT
+-- 	servant_name AS Diener,
+--     product_name AS "Irgendwas mit Sauce oder Lachs"
+-- FROM design.purchases
+-- INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+-- INNER JOIN design.products ON design.products.id = design.purchases.products_id
+-- -- WHERE product_name LIKE "%Lachs%" OR product_name LIKE "%Sauce%" -- IN() ??
+-- WHERE product_name LIKE "%Huhn%"
+-- ;
 
+# Wie oft wurde das Produkt X gekauft?
+-- SELECT
+-- 	product_name AS Produkte,
+--     COUNT(product_name) AS Anzahl
+-- FROM design.purchases
+-- INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+-- INNER JOIN design.products ON design.products.id = design.purchases.products_id
+-- GROUP BY product_name
+-- ORDER BY Anzahl DESC
+-- ;
 
-
-
--- Wieviele Produkte hat X gekauft?
--- "X kauft Y Produkte"
-
-
-
-
-
--- Wieviel Geld hat jeder Diener ausgegeben?
-
-
-
-
-
--- Wer hat das Produkt X gekauft?  
--- Irgendwas mit Lachs / Irgendwas mit Sauce LIKE
--- Spalten --> Diener / Produkt
--- WHERE / LIKE
-
-
-
-
-
--- Wie oft wurde das Produkt X gekauft?
-
-
-
-
--- Welche Umsätze hatte das Produkt X?
+# Welche Umsätze hatte das Produkt X?
+SELECT
+	product_name AS Produkte, -- nicht aggr.
+    COUNT(product_name) AS Anzahl, -- aggr.
+    SUM(product_price) AS Umsaetze-- aggr. // count() * price
+FROM design.purchases
+INNER JOIN design.servants ON design.servants.id = design.purchases.servants_id
+INNER JOIN design.products ON design.products.id = design.purchases.products_id
+GROUP BY product_name
+ORDER BY Umsaetze DESC
+;
